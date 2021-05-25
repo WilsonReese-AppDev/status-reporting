f6ddb6bac70e3a5e58bf4cc85f0a16af5b4c0d68
WilsonReese <reese@chicagobooth.edu>
2021-05-25 18:40:25 +0000

    added current boolean to periods

--- app/models/period.rb
+++ app/models/period.rb
@@ -8,6 +8,7 @@
 #  reports_count :integer          default(0)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
+#  current       :boolean          default(FALSE)
 #
 class Period < ApplicationRecord
   has_many :reports, dependent: :destroy
