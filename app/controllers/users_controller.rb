7e8bcde37e9a3901f6e2534aaff9c42059c7438d
WilsonReese <reese@chicagobooth.edu>
2021-05-25 16:40:39 +0000

    bench status changes onchange

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       if @user.update(user_params)
         format.html { redirect_to root_url, notice: "Bench status was successfully updated." }
         format.json { render :show, status: :ok, location: @report }
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @report.errors, status: :unprocessable_entity }
